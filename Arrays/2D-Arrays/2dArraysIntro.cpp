8c2802e90235ff4ad09808028d0c0996fadf8352
shivanshu1177 <shivanshu1177@chitkara.edu.in>
2022-09-11 23:35:12 +0530

    learning

--- Arrays/2D-Arrays/2dArraysIntro.cpp
+++ Arrays/2D-Arrays/2dArraysIntro.cpp
@@ -4,13 +4,24 @@ using namespace std;
 int main(){
     //creating a 2d array
     int arr[3][4];
+    // int arr[3][4] = {1,2,3,4,5,6,7,8,9,10,11,12};
+    // int arr[3][4] = {{1,11,111,1111},{2,22,222,2222,2222},{3,33,333,3333}};
 
-    //input in 2d array
-    for(int i = 0; i <3; i++){  
+    //row vise input in 2d array
+
+    /*for(int i = 0; i <3; i++){  
         for(int j = 0; j< 4; j++){
             cin>> arr[i][j];
         }
-    }
+    }*/
+
+    //col vise input
+
+    /*for(int j = 0; j<4; j++){
+        for(int i = 0; i< 3; i++){
+            cin>> arr[i][j];
+        }
+    }*/
 
     //output 
     cout<< endl;
