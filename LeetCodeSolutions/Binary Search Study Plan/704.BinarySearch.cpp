a6c0bf72460a60b8ff04c253e419810507aed7b3
shivanshu1177 <shivanshu1177@chitkara.edu.in>
2022-09-13 03:27:49 +0530

    practice

---
+++ LeetCodeSolutions/Binary Search Study Plan/704.BinarySearch.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+
+        int start = 0;
+        int end = n - 1;
+        int mid = start + (end - start) / 2;
+        while (start <= end)
+        {
+            if (nums[mid] == target)
+            {
+                return mid;
+            }
+            if (nums[mid] < target)
+            {
+                start = mid + 1;
+            }
+            if (nums[mid] > target)
+            {
+                end = mid - 1;
+            }
+
+            mid = start + (end - start) / 2;
+        }
+        return -1;
+    }
+};
